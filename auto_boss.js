ab56280a8a5f3a708293f795b4677534a4904245
Dmitriy Minaev <part2@mail.ru>
2017-08-24 17:28:15 +0300

    Create auto_boss.js

---
+++ auto_boss.js
@@ -0,0 +1,20 @@
+
+// ==UserScript==
+// @name           King auto
+// @namespace      https://github.com/VELIKII-DIVAN/
+// @description    Sender for Tower of Knowledge
+// @match          http://www.nutaku.net/games/kingdoms-of-lust/play/*
+// @match          https://www.nutaku.net/games/kingdoms-of-lust/play/*
+// @version        0.0
+// @updateURL      https://github.com/cooljingle/VELIKII-DIVAN/king_auto/auto_boss.js
+// @downloadURL    https://github.com/cooljingle/VELIKII-DIVAN/king_auto/auto_boss.js
+// @grant          none
+// ==/UserScript==
+
+alert('King auto is loaded1');
+$(document).ready(function() {
+  alert('King auto is loaded2');
+};
+
+
+alert('King auto is loaded3');
